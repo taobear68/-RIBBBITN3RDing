c49fdbefef250d11c52b1392d55d2ddffe931b53
Paul Tourville <tourvilp@vm-um1804-jobhunt-01.fairweather.ribbbitmedia.com>
2023-01-29 09:28:35 -0500

    Added a code refresh script which, frankly is long overdue.

---
+++ rmvod/bash/code_refresh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Cloning the repo..."
+cd && mkdir -p git && \
+pushd ~/git && \
+rm -rf ./-RIBBBITN3RDing && \
+git clone https://github.com/taobear68/-RIBBBITN3RDing.git && \
+popd && \
+date
+
+echo "Refreshing deployed code..."
+pushd ~/git/-RIBBBITN3RDing/rmvod/ && \
+cp js/* /var/www/html/rmvod/js/ && \
+cp html/* /var/www/html/rmvod/ && \
+cp css/* /var/www/html/rmvod/css/ && \
+cp py/* /var/lib/rmvod/py/ && \
+popd && \
+date
+
+echo "The rmvod API will need to be manually restarted for all changes to take effect."
+
+
